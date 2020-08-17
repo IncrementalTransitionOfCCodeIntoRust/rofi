ae7a84c0be80ac215faa8699c28efa0607cdb6e4
Philip Koslowski <philip.koslowski@gmail.com>
2020-08-17 11:49:29 +0200

    removed accidentally added pub_functions targets

--- rust/src/pub_functions/target/rls/debug/deps/pub_functions-692d6964379210fb.d
+++
@@ -1,5 +0,0 @@
-/home/phil/development/thesis/trendingcrepos/rofi/rust/src/pub_functions/target/rls/debug/deps/pub_functions-692d6964379210fb.rmeta: src/lib.rs
-
-/home/phil/development/thesis/trendingcrepos/rofi/rust/src/pub_functions/target/rls/debug/deps/pub_functions-692d6964379210fb.d: src/lib.rs
-
-src/lib.rs:
--- rust/src/pub_functions/target/rls/debug/deps/pub_functions-74a97618de615aff.d
+++
@@ -1,5 +0,0 @@
-/home/phil/development/thesis/trendingcrepos/rofi/rust/src/pub_functions/target/rls/debug/deps/pub_functions-74a97618de615aff.rmeta: src/lib.rs
-
-/home/phil/development/thesis/trendingcrepos/rofi/rust/src/pub_functions/target/rls/debug/deps/pub_functions-74a97618de615aff.d: src/lib.rs
-
-src/lib.rs:
